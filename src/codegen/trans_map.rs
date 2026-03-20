131b68ba3845793bc17365df00a728d699dda208
Gianfranco Baccarella <90854992+RedFoXalabama@users.noreply.github.com>
2026-03-20 16:58:46 +0100

    Extract text formatting to module
    ✅ [Bold, italics and underlining](https://www.overleaf.com/learn/latex/Bold%2C_italics_and_underlining)

--- src/codegen/trans_map.rs
+++ src/codegen/trans_map.rs
@@ -1,7 +1,9 @@
 ﻿use crate::codegen;
-use crate::latex_semantic::{ArgItemNode, CommandNode, OptionalArgNode, RequiredArgNode, TextNode};
+use crate::latex_semantic::{ArgItemNode, CommandNode, OptionalArgNode, RequiredArgNode};
 use std::collections::HashMap;
 use std::sync::OnceLock;
+
+mod text_formatting;
 
 // FUNZIONE PER I COMMAND
 type TranslationFn = fn(Vec<RequiredArgNode>, Vec<OptionalArgNode>) -> String;
@@ -15,7 +17,9 @@ static TRANS_MAP: OnceLock<HashMap<&'static str, TranslationFn>> = OnceLock::new
 fn get_trans_map() -> &'static HashMap<&'static str, TranslationFn> {
     TRANS_MAP.get_or_init(|| {
         let mut m = HashMap::new();
-        m.insert("textbf", render_bold as TranslationFn);
+        m.insert("textbf", text_formatting::render_bold as TranslationFn);
+        m.insert("textit", text_formatting::render_italic as TranslationFn);
+        m.insert("underline", text_formatting::render_underline as TranslationFn);
         m
     })
 }
@@ -26,33 +30,21 @@ pub fn translate_command(command: &CommandNode) -> Option<String> {
         .map(|f| f(command.required_args.clone(), command.optional_args.clone()))
 }
 
-// ------------------------------------- UTILS FUNCTION --------------------------------------------
+// -------------------------------------------------------------------------------------------------
 
 fn out_of_bounds_reqs_arg(reqs: &[RequiredArgNode], start: usize) -> String {
     let mut extra = String::new();
     for req in reqs.iter().skip(start) {
-        extra.push_str(&render_sequence(&req.items));
+        extra.push_str(&render_args_item(&req.items));
     }
     extra
 }
 
-// ------------------------------- TRANSLATION FUNCTION --------------------------------------------
-fn render_bold(reqs: Vec<RequiredArgNode>, opts: Vec<OptionalArgNode>) -> String {
-    let mut out = String::new();
-    if let Some(first) = reqs.first() {
-        out.push_str(&format!("*{}*", render_sequence(&first.items)));
-    }
-
-    // metto in coda gli altri elementi in modo che rispetti l'ordine dell'input
-    out.push_str(&out_of_bounds_reqs_arg(&reqs, 1));
-    out
-}
-
-fn render_sequence(seq: &Vec<ArgItemNode>) -> String {
+fn render_args_item(seq: &Vec<ArgItemNode>) -> String {
     seq.into_iter()
         .map(|item| match item {
             ArgItemNode::Command(cmd) => codegen::render_command(&cmd),
-            ArgItemNode::Group(group) => render_sequence(&group.items),
+            ArgItemNode::Group(group) => render_args_item(&group.items),
             ArgItemNode::Newlines(newlines) => "\n".repeat(newlines.count),
             ArgItemNode::Text(text) => codegen::render_text(&text),
         })
---
+++ src/codegen/trans_map/text_formatting.rs
@@ -0,0 +1,35 @@
+﻿use crate::codegen::trans_map::{out_of_bounds_reqs_arg, render_args_item};
+use crate::latex_semantic::{OptionalArgNode, RequiredArgNode};
+
+pub fn render_bold(reqs: Vec<RequiredArgNode>, opts: Vec<OptionalArgNode>) -> String {
+    let mut out = String::new();
+    if let Some(first) = reqs.first() {
+        out.push_str(&format!("*{}*", render_args_item(&first.items)));
+    }
+
+    // metto in coda gli altri elementi in modo che rispetti l'ordine dell'input
+    out.push_str(&out_of_bounds_reqs_arg(&reqs, 1));
+    out
+}
+
+pub fn render_italic(reqs: Vec<RequiredArgNode>, opts: Vec<OptionalArgNode>) -> String {
+    let mut out = String::new();
+    if let Some(first) = reqs.first() {
+        out.push_str(&format!("_{}_", render_args_item(&first.items)));
+    }
+
+    // metto in coda gli altri elementi in modo che rispetti l'ordine dell'input
+    out.push_str(&out_of_bounds_reqs_arg(&reqs, 1));
+    out
+}
+
+pub fn render_underline(reqs: Vec<RequiredArgNode>, opts: Vec<OptionalArgNode>) -> String {
+    let mut out = String::new();
+    if let Some(first) = reqs.first() {
+        out.push_str(&format!("#underline[{}]", render_args_item(&first.items)));
+    }
+
+    // metto in coda gli altri elementi in modo che rispetti l'ordine dell'input
+    out.push_str(&out_of_bounds_reqs_arg(&reqs, 1));
+    out
+}
